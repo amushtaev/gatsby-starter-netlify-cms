0ebb5d391671f6706142e54f673de2ac25d3ea48
amushtaev <ms@adesign.dp.ua>
2020-06-24 10:05:57 +0300

    add industries page

---
+++ src/cms/preview-templates/IndustriesPagePreview.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { IndustriesPageTemplate } from '../../templates/industries-page'
+
+const AboutPagePreview = ({ entry, widgetFor }) => (
+  <IndustriesPageTemplate
+    title={entry.getIn(['data', 'title'])}
+    content={widgetFor('body')}
+  />
+)
+
+IndustriesPagePreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func,
+  }),
+  widgetFor: PropTypes.func,
+}
+
+export default IndustriesPagePreview
