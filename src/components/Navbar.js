b8772831188220ac7807b84f942158f7470cc25d
amushtaev <ms@adesign.dp.ua>
2020-06-24 10:20:33 +0300

    add industries page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,6 +60,9 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
+              <Link className="navbar-item" to="/industries">
+                Industries
+              </Link>
               <Link className="navbar-item" to="/about">
                 About
               </Link>
