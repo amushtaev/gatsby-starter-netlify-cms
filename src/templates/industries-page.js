6246f7e03d49b8babdc90172cf59e85b2ab320d3
amushtaev <ms@adesign.dp.ua>
2020-06-24 09:29:50 +0300

    add industries page

---
+++ src/templates/industries-page.js
@@ -0,0 +1,5 @@
+import React from "react"
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
