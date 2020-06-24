f3f1275c9cd6d45d1bec2bf17228db47caab747c
amushtaev <ms@adesign.dp.ua>
2020-06-24 09:42:08 +0300

    add industries page

--- src/templates/industries-page.js
+++ src/templates/industries-page.js
@@ -3,3 +3,60 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+
+export const IndustriesPageTemplate = ({title, content, contentComponent}) => {
+  const PageContent = contentComponent || Content
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="columns">
+          <div className="column is-10 is-offset-1">
+            <div className="section">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {title}
+              </h2>
+              <PageContent className="content" content={content} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+IndustriesPageTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  contentComponent: PropTypes.func,
+}
+
+const IndustriesPage = ({ data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout>
+      <IndustriesPageTemplate
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
+        content={post.html}
+      />
+    </Layout>
+  )
+}
+
+IndustriesPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default IndustriesPage
+
+export const aboutPageQuery = graphql`
+  query IndustriesPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
