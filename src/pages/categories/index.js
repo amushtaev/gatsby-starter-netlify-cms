d4d0c33d3b8862a834d22ea513fe1c33756b0102
amushtaev <ms@adesign.dp.ua>
2020-06-30 09:24:27 +0300

    Add Category in admin Page

--- src/pages/categories/index.js
+++ src/pages/categories/index.js
@@ -1,11 +1,31 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import {graphql, Link} from 'gatsby'
 import Layout from '../../components/Layout'
+import {Helmet} from "react-helmet";
+import {kebabCase} from "lodash";
 
-const CategoriesPage = () => {
+const CategoriesPage = ({
+  data: {
+    allMarkdownRemark: { group },
+    site: {
+      siteMetadata: { title },
+    },
+  },
+}) => {
+  console.log(group, "group CategoriesPage")
   return (
     <Layout>
-      <button>Hello world</button>
+      <Helmet title={`Categories | ${title}`} />
+      <h1 className="title is-size-2 is-bold-light">Categories</h1>
+      <ul className="taglist">
+        {group.map((catygory) => (
+          <li key={catygory.fieldValue}>
+            <Link to={`/tags/${kebabCase(catygory.fieldValue)}/`}>
+              {catygory.fieldValue} ({catygory.totalCount})
+            </Link>
+          </li>
+        ))}
+      </ul>
     </Layout>
   )
 };
